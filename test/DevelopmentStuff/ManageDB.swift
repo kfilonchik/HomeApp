bf10737f10d6be4f2888fa456669c29c2e0195c9
Mathis <majokto@gmail.com>
2018-07-14 01:49:28 +0200

    add devices to tiles via manage db

--- test/DevelopmentStuff/ManageDB.swift
+++ test/DevelopmentStuff/ManageDB.swift
@@ -20,10 +20,8 @@ class ManageDB: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         aConnector.startUpConnector()
-        
     }
 
-    
     @IBAction func addWrongData(_ sender: UIButton) {
         let stdAppSettings = AppSettings(context: context)
         stdAppSettings.fritzID = "rtzrtz"
@@ -37,7 +35,6 @@ class ManageDB: UIViewController {
             print(error)
         }
     }
-    
     
     @IBAction func printBtn(_ sender: UIButton) {
         let fetchRequest: NSFetchRequest<AppSettings> = AppSettings.fetchRequest()
@@ -56,7 +53,6 @@ class ManageDB: UIViewController {
         }
  */
         
-        
     }
     
     @IBAction func deleteContentBtn(_ sender: UIButton) {
@@ -69,19 +65,119 @@ class ManageDB: UIViewController {
         
         // Create Batch Delete Request
         let batchDeleteRequest = NSBatchDeleteRequest(fetchRequest: fetchRequest as! NSFetchRequest<NSFetchRequestResult>)
-        
         do {
             try context.execute(batchDeleteRequest)
             
         } catch {
             // Error Handling
         }
-        
         context.reset()
     }
-
-
     
+    @IBAction func addDevices(_ sender: UIButton) {
+        
+        let switchRequest: NSFetchRequest<SwitchDevice> = SwitchDevice.fetchRequest()
+        let switches = try? context.fetch(switchRequest)
+        
+        let thermosRequest: NSFetchRequest<Thermostat> = Thermostat.fetchRequest()
+        let thermos = try? context.fetch(thermosRequest)
+        
+
+        
+        if (switches != nil)
+        {
+            for aswitch in switches!{
+                let aswitchTile = SwitchTile(context: context)
+                aswitch.tile = aswitchTile
+                aswitchTile.switchDevice = aswitch
+            }
+        }
+        
+        if (thermos != nil)
+        {
+            for athermo in thermos!{
+                let aThermoTile = ThermostatTile(context: context)
+                athermo.tile = aThermoTile
+                aThermoTile.thermostat = athermo
+            }
+        }
+        
+        do{ // persist data
+            try context.save()
+            
+        } catch {
+            print(error)
+        }
+        print("Alle Devices zu Kacheln erstellt")
+        
+    }
+    
+    @IBAction func removeDevices(_ sender: UIButton) {
+        
+        print("delete procedure")
+        let fetchRequest: NSFetchRequest<SwitchTile> = SwitchTile.fetchRequest()
+        
+        // Create Batch Delete Request
+        let batchDeleteRequest = NSBatchDeleteRequest(fetchRequest: fetchRequest as! NSFetchRequest<NSFetchRequestResult>)
+        do {
+            try context.execute(batchDeleteRequest)
+            
+        } catch {
+            // Error Handling
+        }
+        context.reset()
+        
+        let fetchRequest2: NSFetchRequest<ThermostatTile> = ThermostatTile.fetchRequest()
+        let batchDeleteRequest2 = NSBatchDeleteRequest(fetchRequest: fetchRequest2 as! NSFetchRequest<NSFetchRequestResult>)
+        do {
+            try context.execute(batchDeleteRequest2)
+            
+        } catch {   
+            // Error Handling
+        }
+        context.reset()
+        
+    }
+    
+    @IBAction func printSTT(_ sender: UIButton) {
+        
+        let switchTilesRequest: NSFetchRequest<SwitchTile> = SwitchTile.fetchRequest()
+        let switchTiles = try? context.fetch(switchTilesRequest)
+        
+        print("Anzahl Kacheln switches \(switchTiles!.count)")
+      
+        
+        for bla in switchTiles!{
+            print("Eine Kachel mit Switch: \(bla.switchDevice?.title)")
+        }
+        
+        let thermoTilesRequest: NSFetchRequest<ThermostatTile> = ThermostatTile.fetchRequest()
+        let thermoTiles = try? context.fetch(thermoTilesRequest)
+        
+        print("Anzahl Kacheln thermostate \(thermoTiles!.count)")
+        
+        for bla in thermoTiles!{
+            print("Eine Kachel mit Thermostat:  \(bla.thermostat?.title)")
+        }
+        
+        /*
+        let switchesRequest: NSFetchRequest<SwitchDevice> = SwitchDevice.fetchRequest()
+        let switches = try? context.fetch(switchesRequest)
+        
+        for bla in switches!{
+            print("Ein Switch: \(bla.tile?.title)")
+        }
+        let thermosRequest: NSFetchRequest<Thermostat> = Thermostat.fetchRequest()
+        let thermos = try? context.fetch(thermosRequest)
+        
+        for bla in thermos!{
+            print("Ein Thermostat: \(bla.tile?.title)")
+        }
+        */
+        
+    }
+    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
