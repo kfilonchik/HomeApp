fbd3e54cc5528262eab870931bb76e6fb08ffa78
Khris <filnch92@gmail.com>
2018-07-17 19:38:34 +0200

    Problems with Tiles, #Bug1. I can get titles of thermostat in tiles only at once. if i restart my app, it doesn't work anymore

--- Controller/ViewController.swift
+++ Controller/ViewController.swift
@@ -12,6 +12,8 @@ import CoreData
 
 class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
     
+    
+    let context = AppDelegate.viewContext
     let reuseIdentifier = "cell1"
     let reuseIdentifier1 = "cell2"
     let items = ["Etwas","Termostat", "Schalter"]
@@ -20,11 +22,52 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
     
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return self.items.count
+        let dashboardTilesRequest: NSFetchRequest<DashboardTile> = DashboardTile.fetchRequest()
+        let dashboardTiles = try? context.fetch(dashboardTilesRequest)
+        print(dashboardTiles!.count)
+        //return dashboardTiles!.count
+        return 5
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let thermoTilesRequest: NSFetchRequest<ThermostatTile> = ThermostatTile.fetchRequest()
+        let thermoTiles = try? context.fetch(thermoTilesRequest)
         
+        if (thermoTiles != nil)
+        {
+            var names: [String] = []
+            
+            for bla in thermoTiles!{
+                 print("Eine Kachel mit Thermostat:  \(bla.thermostat?.title)")
+                
+                names.append((bla.thermostat?.title)!)
+            }
+ 
+            let cell1 = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath as IndexPath) as! CollectionCellViewController
+            cell1.titel1.text = names[indexPath.item]
+            return cell1
+        }
+        else {
+            let cell1 = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier1, for: indexPath as IndexPath) as! CollectionCellViewController
+            return cell1
+        }
+    }
+      
+        /*
+        var titles = [String]()
+        print(titles)
+        print("Anzahl Kacheln thermostate \(thermoTiles!.count)")
+        for bla in thermoTiles!{
+            titles.append((bla.thermostat?.title)!)
+        }
+        let cell1 = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath as IndexPath) as! CollectionCellViewController
+        
+        cell1.titel1.text = titles[indexPath.item]
+        return cell1
+ */
+  
+        
+        /*
         if(indexPath.item > 0){
              let cell1 = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath as IndexPath) as! CollectionCellViewController
             /*do setup here*/
@@ -36,8 +79,10 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
             
             return cell2
         }
+        
+        */
         // Use the outlet in our custom class to get a reference to the UILabel in the cell
-    }
+  
     /*
     func collectionView(_: UICollectionView, canPerformAction: Selector, cellForItemAt indexPath: IndexPath, withSender: Any?)  {
         
